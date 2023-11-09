7473bee5a086e71fa8b8e48c04541cf30afa4a09
bruno <brunoqueiroz612@gmail.com>
2023-11-09 10:15:08 -0300

    Create quiz controller

---
+++ src/main/java/com/quiz/quiz/controllers/QuizController.java
@@ -0,0 +1,22 @@
+package com.quiz.quiz.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.quiz.quiz.models.Quiz;
+import com.quiz.quiz.services.QuizServices;
+
+@RestController
+@RequestMapping("quiz")
+public class QuizController {
+    
+    @Autowired
+    private QuizServices quizServices;
+
+    @PostMapping
+    public Quiz createQuiz(Quiz quiz){
+        return quizServices.createQuiz(quiz);
+    }
+}
