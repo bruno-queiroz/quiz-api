799f3e06f8701ea5eeee7503bf611aeea2710c82
bruno <brunoqueiroz612@gmail.com>
2023-11-16 10:01:03 -0300

    Create answerQuiz service

--- src/main/java/com/quiz/quiz/services/QuizServices.java
+++ src/main/java/com/quiz/quiz/services/QuizServices.java
@@ -1,10 +1,15 @@
 package com.quiz.quiz.services;
 
+import java.util.Optional;
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.quiz.quiz.dtos.AnswerQuizInputDto;
 import com.quiz.quiz.models.Quiz;
 import com.quiz.quiz.repositories.QuizRepository;
+import com.quiz.quiz.utils.AnswerPayload;
 
 @Service
 public class QuizServices {
@@ -13,5 +18,21 @@ public class QuizServices {
 
     public Quiz createQuiz(Quiz quiz){
         return quizRepository.save(quiz);
+    }
+
+    public AnswerPayload answerQuiz(AnswerQuizInputDto answer, UUID quizId){
+        Optional<Quiz> quizOptional = quizRepository.findById(quizId);
+
+        if(quizOptional.isPresent()){
+            Quiz quiz = quizOptional.get();
+
+            if(quiz.getAnswer() == answer.answer()){
+                return new AnswerPayload(quiz, true);
+            } else{
+                return new AnswerPayload(quiz, false);
+            }
+        }
+
+        return new AnswerPayload(null, false); 
     }
 }
