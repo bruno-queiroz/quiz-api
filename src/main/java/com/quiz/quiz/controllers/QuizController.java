fda2e0216d255daeb07f1dc7e431fda9dfa00c82
bruno <brunoqueiroz612@gmail.com>
2023-11-16 09:59:47 -0300

    Create answer endpoint

--- src/main/java/com/quiz/quiz/controllers/QuizController.java
+++ src/main/java/com/quiz/quiz/controllers/QuizController.java
@@ -1,13 +1,21 @@
 package com.quiz.quiz.controllers;
 
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.quiz.quiz.dtos.AnswerQuizInputDto;
 import com.quiz.quiz.models.Quiz;
+import com.quiz.quiz.response.ResponseHandler;
 import com.quiz.quiz.services.QuizServices;
+import com.quiz.quiz.utils.AnswerPayload;
 
 @RestController
 @RequestMapping("quiz")
@@ -19,5 +27,19 @@ public class QuizController {
     @PostMapping
     Quiz createQuiz(@RequestBody Quiz quiz){
         return quizServices.createQuiz(quiz);
+    }
+
+    @PostMapping("/answer/{quizId}")
+    ResponseEntity answerQuiz(@PathVariable UUID quizId, @RequestBody AnswerQuizInputDto answer){
+        AnswerPayload data = quizServices.answerQuiz(answer, quizId);
+        if(data.getData() == null){
+            return ResponseHandler.generateResponse(null, HttpStatus.NOT_FOUND, "Question not found.", false);
+        }
+
+        if(data.getIsCorrect() == false){
+            return ResponseHandler.generateResponse(data.getData(), HttpStatus.CREATED, "Wrong answer!", true);
+
+        }
+        return ResponseHandler.generateResponse(data, HttpStatus.CREATED, "Right answer!", true);
     }
 }
