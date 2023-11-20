dba2118e5be1217536b17956be694b2e0469badb
bruno <brunoqueiroz612@gmail.com>
2023-11-20 09:52:31 -0300

    Create deleteQuiz controller

--- src/main/java/com/quiz/quiz/controllers/QuizController.java
+++ src/main/java/com/quiz/quiz/controllers/QuizController.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -47,5 +48,11 @@ public class QuizController {
 
         }
         return ResponseHandler.generateResponse(data, HttpStatus.CREATED, "Right answer!", true);
+    }
+
+    @DeleteMapping("/{quizId}")
+    ResponseEntity<Object> deleteQuiz(@PathVariable UUID quizId){
+        quizServices.deleteQuiz(quizId);
+        return ResponseHandler.generateResponse(quizId, HttpStatus.OK, "Quiz deleted.", true);
     }
 }
